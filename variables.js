59f2c574da94d511681c7163121df16030d15b0f
Muhammad Ahsan Mushtaq <162441409+ahsan425615@users.noreply.github.com>
2024-03-07 07:26:08 +0000

    this is variables

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountemail = "abc@gmail.com"
+var accountpass = "123456"
+accountcity = "pindi"
+
+console.log(accountId);
+console.table([accountId, accountemail, accountpass]);
